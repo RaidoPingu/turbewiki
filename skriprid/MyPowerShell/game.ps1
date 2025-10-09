ca450e651ff1fbdb9b3a2fe9a96e4d63ff7e1b67
RaidoPingu <suurmets@protonmail.com>
2025-10-09 11:51:15 +0300

    Base game logic done

--- skriprid/MyPowerShell/game.ps1
+++ skriprid/MyPowerShell/game.ps1
@@ -15,6 +15,35 @@ function writeToFile{
     ($name, $Global:steps -join ";") | Out-File -FilePath $filename -Append
 }
 
+function Show-Scoreboard {
+    $content = Get-Content -Path $filename
+    foreach($line in $content) {
+        Write-Host $line.Split(";")
+    }
+}
 
+function LetsPlay{
+    [int]$user_nr = Read-Host " Sisesta number"
+    $Global:steps++ #Global:steps +=1
+    $game_over = $false
 
+    if (($user_nr -gt $pc_nr) -and ($user_nr -ne 1000)) {
+        Write-Host "Võiksem"
+    } elseif(($user_nr -lt $pc_nr) -and ($user_nr -ne 1000)){
+        Write-Host "Suurem"
+    } elseif(($user_nr -eq $pc_nr) -and ($user_nr -ne 1000)){
+        Write-Host "Leidsid õige numbri $Global:steps sammuga"
+        $game_over = $true
+    } elseif($user_nr -eq 1000) {
+        Write-Host "Leidsid mu nõrga koha. Õige number on $pc_nr"
+    }
+    return $game_over #Tagastab tulemuse $true või $false
+}
+
+while($game_over -eq $false){
+    $game_over = LetsPlay
+}
+
+#LetsPlay
 #writeToFile
+#Show-Scoreboard
