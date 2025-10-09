78672b82e7cfa6f890faff7432aa72700b356916
RaidoPingu <suurmets@protonmail.com>
2025-10-09 10:16:59 +0300

    Katse

--- skriprid/MyPowerShell/esimene.ps1
+++ skriprid/MyPowerShell/esimene.ps1
@@ -1,2 +1,15 @@
+<#
+Siia vabalt mitmerealine kommentaar
+#>
+
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 Clear-Host #Puhasta ekraan
+Get-Date #Näita tänast kuupäeva
+
+$username = Read-Host -Prompt "Sisesta enda kasutajanimi"
+
+if($username -eq $env:USERNAME){
+    Write-Host "Õige kasutajanimi"
+} else {
+     Write-Host "Vale kasutajanimi $username, eeldati $env:USERNAME "
+}
